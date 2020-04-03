3590f416ae1376ac37c91aed7b28153574bb2d3d
Rob Merritt <rob.merritt@usask.ca>
2020-04-02 21:56:56 -0600

    update geolocate

--- src/App.js
+++ src/App.js
@@ -2,31 +2,39 @@
 // @flow
 
 import React, { Component } from 'react'
+
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import * as hydroData from "./data/hydrometric.json";
 import "./App.css";
 import "./leaflet.css"
 import Leaflet from 'leaflet'
+import { geolocated } from "react-geolocated";
 Leaflet.Icon.Default.imagePath =
   '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/'
+  
 
-type State = {
-  lat: number,
-  lng: number,
-  zoom: number,
-}
 
-export default class SimpleExample extends Component<{}, State> {
-  state = {
-    lat: 52.1332,
-    lng: -106.6700,
-    zoom: 13, 
-  }
+// type State = {
+//   lat: number,
+//   lng: number,
+//   zoom: number,
+// }
 
+export /*default*/ class Hydro extends Component<{}, State, GeolocatedProps> {
+  // state = {
+  //   lat: 52.1332,
+  //   lng: -106.6700,
+  //   zoom: 13, 
+  // }
+  
   render() {
-    const position = [this.state.lat, this.state.lng]
+    //const position = [this.state.lat, this.state.lng]
+    //const position = [{this.props.coords.latitude}, {this.props.coords.longitude}]
+    const position = [52.1332, -106.6700]
+    
     return (
 
-      <Map center={position} zoom={this.state.zoom}>
+      <Map center={position} zoom={5}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -40,3 +48,10 @@ export default class SimpleExample extends Component<{}, State> {
     )
   }
 }
+
+ export default geolocated({
+   positionOptions: {
+       enableHighAccuracy: false,
+   },
+   userDecisionTimeout: 5000,
+ })(Hydro);
